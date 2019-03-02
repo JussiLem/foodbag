e6af2c27ab83b57691c9542295f9ddf07d6cba0b
Jussi <jussi.lem@gmail.com>
2019-03-02 21:02:28 +0200

    navbar for the app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,31 @@ import { Link } from "react-router-dom";
 import { Navbar } from "react-bootstrap";
 import Routes from "./Routes";
 import "./App.css";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 class App extends Component {
     render() {
         return (
             <div className="App container">
-                <Navbar fluid collapseOnSelect>
-
-                        <Navbar.Brand>
-                            <Link to="/">FoodBag</Link>
-                        </Navbar.Brand>
-                        <Navbar.Toggle />
-
+                <Navbar bg="light" expand="lg">
+                    <Navbar.Brand>
+                        <Link to="/">FoodBag</Link>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse>
+                        <Nav className="mr-auto">
+                            <Nav.Link href="/siqnup">Signup</Nav.Link>
+                            <Nav.Link href="/login">Login</Nav.Link>
+                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Navbar>
                 <Routes />
             </div>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,22 +5,13 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "./index.css";
 
-const root = document.getElementById('root');
-if (root !== null) {
-    ReactDOM.render(
-        <Router>
-            <App/>
-        </Router>, root
-    )
-}
-/*
 ReactDOM.render(
     <Router>
         <App />
     </Router>,
     document.getElementById("root")
 );
-*/
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
