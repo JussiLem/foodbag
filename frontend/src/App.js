82361e8bb03bf9e92c6c390d30f9b7233d3ef176
Jussi <jussi.lem@gmail.com>
2019-03-02 21:12:11 +0200

    more fixes for the navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { Navbar } from "react-bootstrap";
+import { Nav, Navbar, NavDropdown, NavItem } from "react-bootstrap";
 import Routes from "./Routes";
 import "./App.css";
-import Nav from "react-bootstrap/Nav";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import { LinkContainer } from "react-router-bootstrap";
 
 class App extends Component {
     render() {
@@ -16,9 +15,13 @@ class App extends Component {
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse>
-                        <Nav className="mr-auto">
-                            <Nav.Link href="/siqnup">Signup</Nav.Link>
-                            <Nav.Link href="/login">Login</Nav.Link>
+                        <Nav className="navbar-nav ml-auto">
+                            <LinkContainer to="/signup">
+                                <NavItem>Signup</NavItem>
+                            </LinkContainer>
+                            <LinkContainer to="/login">
+                                <NavItem>Login</NavItem>
+                            </LinkContainer>
                             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
